0b747e14334b61465404486d9f48de88309bdb71
pasipiya <piyathilaka10@gmail.com>
2021-08-09 20:44:27 +0530

    Write data to json file

--- game_api.php
+++ game_api.php
@@ -5,7 +5,14 @@
   if (empty($gameResults)) {
     echo "Name is empty";
   } else {
-    echo $gameResults;
-  }
+    // encode array to json
+    $json = json_encode($gameResults);
+
+    //write json to file
+    if (file_put_contents("data.json", $json))
+        echo "JSON file created successfully...";
+    else 
+        echo "Oops! Error creating json file...";
+    }
 
 ?>
