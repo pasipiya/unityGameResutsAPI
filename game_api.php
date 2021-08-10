5c7ac8fa75b05f361aad9aed9b055ee8f06d77a6
pasipiya <piyathilaka10@gmail.com>
2021-08-10 19:41:01 +0530

    Save multiple game results in single json file

--- game_api.php
+++ game_api.php
@@ -1,18 +1,38 @@
 <?php
+  if (empty($_POST['id'])) {
+    echo "Game reslts are empty";
+  } elseif(file_exists("game1.json")) {
+    $id = $_POST['id'];
+    $result = $_POST['result'];
+    $timeStamp = $_POST['timeStamp'];
 
-  // collect value of input field
-  $gameResults = $_POST['gameResults'];
-  if (empty($gameResults)) {
-    echo "Name is empty";
-  } else {
-    // encode array to json
-    $json = json_encode($gameResults);
+    //Get input json
+    $inp = file_get_contents('game1.json');
+    $tempArray = json_decode($inp,true);
 
+    //Build gameResult array
+    $gameResults = [ 
+      "id" => $id,
+      "result" => $result,
+      "timeStamp" => $timeStamp
+    ];
+
+    //Append new game results
+    array_push($tempArray,$gameResults);
+    $jsonData = json_encode($tempArray);
+    echo $jsonData."<br>";
+ 
     //write json to file
-    if (file_put_contents("data.json", $json))
+    if (file_put_contents("game1.json", $jsonData))
         echo "JSON file created successfully...";
     else 
         echo "Oops! Error creating json file...";
-    }
+
+  }else{
+    echo "Json file is not available";
+  }
+   
+
+
 
 ?>
--- post.php
+++ post.php
@@ -2,7 +2,9 @@
 <body>
 
 <form method="post" action="game_api.php">
-  Name: <input type="text" name="gameResults">
+  ID: <input type="text" name="id">
+  Result: <input type="text" name="result">
+  Time Stamp: <input type="text" name="timeStamp">
   <input type="submit">
 </form>
 
